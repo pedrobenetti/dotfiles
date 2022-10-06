9685243fc9504fbc1c83c7b89bde683a473eaaf6
pedrobenetti <pedrobenetti@outlook.com.br>
2022-10-06 02:03:01 -0300

    dotfiles

--- install.sh
+++ install.sh
@@ -46,4 +46,10 @@ cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 
+###########
+# Polybar #
+###########
+
+rm -rf "$XDG_CONFIG_HOME/polybar"
+ln -sf "$DOTFILES/polybar" "$XDG_CONFIG_HOME"
 
